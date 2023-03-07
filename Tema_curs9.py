8e687d3b344bf04ac983b5729e1473cebb4494ee
Raul <albus.raul97@yahoo.ro>
2023-03-07 21:06:25 +0200

    tema 9

--- Tema_curs9.py
+++ Tema_curs9.py
@@ -1,4 +1,9 @@
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class Login(unittest.TestCase):
+    chrome: WebDriver = webdriver.Chrome()
+    chrome.get('https://the-internet.herokuapp.com/')
+    form = chrome.find_element(By.LINK_TEXT, 'Form Authentication').click()
