9c703547d63b5d57ca47d1ce1f37dd8874f52500
Raul266 <124399528+Raul266@users.noreply.github.com>
2023-03-21 21:26:31 +0200

    teste curs 8,10

--- TesteCurs8_10/Class_curs8.py
+++ TesteCurs8_10/Class_curs8.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 from selenium import webdriver
@@ -34,3 +35,23 @@ class LoginTema8(unittest.TestCase):
         actual = self.chrome.current_url
         expected = 'https://phptravels.net/login/failed'
         self.assertEqual(expected, actual, "Te-ai logat cu un cont gresit")
+
+class JulesLogin(unittest.TestCase):
+    def setUp(self):
+        self.chrome: webdriver = webdriver.Chrome()
+        self.chrome.get('https://jules.app/sign-in')
+    def tearDown(self):
+        self.chrome.quit()
+    def login(self,username, password):
+        self.chrome.find_element(By.XPATH,"//input[@autocomplete = 'on'][1]").send_keys(username)
+        self.chrome.find_element(By.XPATH,"//input[@type='password']").send_keys(password)
+    def click_login(self):
+        self.chrome.find_element(By.XPATH,"//div[@class='css-17qmje5']").click()
+    def test3(self):
+        self.login("albus.raul97@yahoo.ro","Raul1243")
+        self.click_login()
+        actual = self.chrome.current_url
+        expected = 'https://jules.app/sign-in'
+        self.assertEqual(expected,actual,'Login cu cont gresit')
+    time.sleep(3)
+
